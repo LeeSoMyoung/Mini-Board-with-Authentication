627eb9f390109593ef86cc002057c98129734632
LeeSoMyoung <so4644009@gmail.com>
2022-02-11 00:42:13 +0900

    test: login 테스트 코드 작성
    sign up 테스트 코드와 마찬가지로 src/test 디렉터리에 테스트 코드를 추가하였다.
    로그인 성공 코드와 실패 코드를 작성하였다.
    sign up이 제대로 동작하지 않아 로그인 성공의 경우가 제대로 테스트 되지 못하지만
    일단 테스트 코드를 추가해놓았다.

---
+++ src/tests/login.spec.js
@@ -0,0 +1,41 @@
+const should = require('should');
+const supertest = require('supertest');
+
+const server = supertest.agent('http://localhost:3000');
+
+describe('로그인 테스트================= ',()=>{
+    it('1. 로그인 실패의 경우',(done)=>{
+        server
+        .post('/login')
+        .send({
+            id:"00000000",
+            pw:"0000000000"
+        })
+        .expect(401)
+        .end((err,res)=>{
+            if(err){
+                done(err);
+            }
+            else{
+                console.log(res.body);
+            }
+        });
+    });
+    it('2. 로그인 성공의 경우',(done)=>{
+        server
+        .post('/login')
+        .send({
+            id: "aaaaaaaaaa",
+            pw: "123456789"
+        })
+        .expect(200)
+        .end((err,res)=>{
+            if(err){
+                done(err);
+            }
+            else{
+                console.log(res.body);
+            }
+        });
+    });
+});
