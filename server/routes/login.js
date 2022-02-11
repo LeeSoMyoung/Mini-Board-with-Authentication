e87e039fbac45be9b129031bc3dc135c60a7c269
LeeSoMyoung <so4644009@gmail.com>
2022-02-11 20:12:36 +0900

    chore: respond message 수정
    로그인을 위해 request를 보낼 때, 존재하지 않는 아이디를 입력하
    아이디나 패스워드가 일치하지 않는다는 응답을 리턴해주었지만,
    정확히 짚어주는게 더 좋을 것 같아서
    '존재하지 않는 유저입니다'를 응답으로 보내주도록 하였다.

--- server/routes/login.js
+++ server/routes/login.js
@@ -9,48 +9,49 @@ const bcrypt = require('bcrypt');
 const db = require('../../src/lib/db.js');
 const userMiddleware = require('../../middlewares/users.js');
 
-router.post('/',(req, res) => {
+router.post('/', (req, res) => {
     db.query(`
         SELECT * FROM USERS
         WHERE ID = ${db.escape(req.body.id)}
     `,
-    (err,result)=>{
-        if(err){
-            throw err;
-            return res.status(400).send({
-                message: err
-            });
-        }
-        if(result===undefined){
-            // 존재하지 않는 유저라면
-            return res.status(401).send({
-                message: "아이디나 비밀번호가 잘못되었습니다."
-            });
-        }
-
-       bcrypt.compare(req.body.pw, result[0]['pw'],
-       (err,result)=>{
-           if(err){
-               // 에러가 발생했다면
-               throw err;
-               return res.status(401).send({
-                   message: "아이디나 패스워드가 일치하지 않습니다."
-               });
-           }
-           // 비번이 일치하면 res에 true, 일치하지 않으면 false가 담긴다.
-           if(result){
-               // 비밀번호가 일치한다면
-               return res.status(200).send({
-                   message: "로그인 성공"
-               });
-           }
-           else{
-               return res.status(403).send({
-                   message: "아이디나 비밀번호가 일치하지 않습니다."
-               });
-           }
-       });
-    });
+        (err, result) => {
+            if (err) {
+                throw err;
+                return res.status(400).send({
+                    message: err
+                });
+            }
+            if (!result.length) {
+                // 존재하지 않는 유저라면
+                return res.status(401).send({
+                    message: "존재하지 않는 유저입니다."
+                });
+            }
+            else {
+                bcrypt.compare(req.body.pw, result[0]['pw'],
+                    (err, result) => {
+                        if (err) {
+                            // 에러가 발생했다면
+                            throw err;
+                            return res.status(401).send({
+                                message: "아이디나 패스워드가 일치하지 않습니다."
+                            });
+                        }
+                        // 비번이 일치하면 res에 true, 일치하지 않으면 false가 담긴다.
+                        if (result) {
+                            // 비밀번호가 일치한다면
+                            return res.status(200).send({
+                                message: "로그인 성공"
+                            });
+                        }
+                        else {
+                            return res.status(403).send({
+                                message: "아이디나 비밀번호가 일치하지 않습니다."
+                            });
+                        }
+                    });
+            }
+        });
 });
 
 
--- src/tests/signup.spec.js
+++ src/tests/signup.spec.js
@@ -4,14 +4,14 @@ const supertest = require('supertest');
 const server = supertest.agent('http://localhost:3000');
 
 describe('회원가입 테스트================= ',()=>{
-    it('회원가입 성공의 경우',(done)=>{
+    it('1. 회원가입 성공의 경우',(done)=>{
         server
         .post('/signup')
         .send({
-            id: "abcdefghijk",
-            pw: "123456789",
-            pw_repeat: "123456789",
-            username: "00000000"
+            "id": "so4644009",
+            "pw": "a123456789",
+            "pw_repeat": "a123456789",
+            "username": "이소명"
         })
         .expect(201)
         .end((err,res)=>{
@@ -23,4 +23,25 @@ describe('회원가입 테스트================= ',()=>{
             }
         });
     });
+
+    it('2. 회원가입 실패 - 아이디와 비밀번호가 너무 짧음',(done)=>{
+        server
+        .post('/signup')
+        .send({
+            "id": "a",
+            "pw": "a",
+            "pw_repeat": "a",
+            "username": "안알랴줌"
+        })
+        .expect(400)
+        .end((err,res)=>{
+            if(err){
+                done(err);
+            }
+            else{
+                console.log(res.body);
+            }
+        });
+    })
+    
 });
