2969a123d0db239eb6fa8cdbb0386715bfee80fa
LeeSoMyoung <so4644009@gmail.com>
2022-02-23 21:22:46 +0900

    feat: SignUpController에 회원가입 정보를 POST 요청으로 보내는 기능 추가
    회원가입 화면에서 클라이언트가 입력한 정보를 가져와서
    POST 요청을 보낸다. 그리고, 옳은 요청인지 user 미들웨어를 통해 체크하고
    올바른 요청이면 DB에 올리고, 올바르지 않은 요청이면 alert 알림창을 띄운다.

--- src/static/js/controllers/SignUpController.js
+++ src/static/js/controllers/SignUpController.js
@@ -2,14 +2,40 @@
 
 import { navigateTo } from "../routes/router.js";
 
-function onSignUpConfirmClick(event) {
+function onSignUpConfirmClick(event, username, id, pw, pw_repeat) {
     event.preventDefault();
-    // TODO : 회원 정보 POST 하는 로직 필요
+
+    fetch('http://localhost:3000/signup', {
+        credentials: "include",
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            "id": id,
+            "pw": pw,
+            "username": username,
+            "pw_repeat": pw_repeat
+        })
+    })
+        .then((res) => {
+            if(res.ok && res.status === 201){
+                navigateTo(location.origin+'/login');
+                return 
+            }
+            else{
+                // 로그인에 실패하면
+               return res.json().then((result)=>{
+                   alert(result.message);
+                   throw new Error(result.message);
+               });
+            }
+        })
+        .catch((err) => {
+            throw new Error(err.message);
+        });
+
     navigateTo(location.origin + '/login');
 }
 
-function postSignUp(req,res){
-
-}
-
-export { onSignUpConfirmClick, postSignUp };
+export { onSignUpConfirmClick };
--- src/static/js/views/SignUpView.js
+++ src/static/js/views/SignUpView.js
@@ -16,12 +16,12 @@ export default class extends AbstractView{
                 <h1 class="form__title">회원가입 하기</h1>
                 <div class="form__message form__message--error"></div>
                 <div class="form__input-group">
-                    <input type="text" class="form__input" autofocus placeholder="닉네임">
-                    <input type="text" class="form__input" autofocus placeholder="아이디">
+                    <input type="text" class="form__input" id="username" autofocus placeholder="닉네임">
+                    <input type="text" class="form__input" id="id" autofocus placeholder="아이디">
                     <div class="form__input-error-message"></div>
-                    <input type="password" class="form__input" autofocus placeholder="비밀번호">
+                    <input type="password" class="form__input" id="pw" autofocus placeholder="비밀번호">
                     <div class="form__input-error-message"></div>
-                    <input type="password" class="form__input" autofocus placeholder="비밀번호 확인">
+                    <input type="password" class="form__input" id="pw_repeat" autofocus placeholder="비밀번호 확인">
                     <div class="form__input-error-message"></div>
                 </div>
                 <button class="form__button" type="submit" id="signup-confirm">회원가입</button>
@@ -33,6 +33,14 @@ export default class extends AbstractView{
     attachEvent(){
         const signUp = document.querySelector('#signup-confirm');
 
-        signUp.addEventListener('click', onSignUpConfirmClick);
+        //// html input들
+        const id = document.querySelector('#id');
+        const pw = document.querySelector('#pw');
+        const pw_repeat = document.querySelector('#pw_repeat');
+        const username = document.querySelector('#username');
+
+        signUp.addEventListener('click', (event)=>{
+            onSignUpConfirmClick(event, username.value, id.value, pw.value, pw_repeat.value);
+        });
     }
 }
