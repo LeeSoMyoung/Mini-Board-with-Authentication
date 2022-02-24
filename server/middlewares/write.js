d77fe18b0dbbf73e64db3bd6457defa8708e8fe4
LeeSoMyoung <so4644009@gmail.com>
2022-02-24 20:33:43 +0900

    feat: 포스팅 관련 라우터 레벨 미들웨어 추가 및 DB 업로드 코드 일부 구현
    포스팅 관련 라우터 레벨 미들웨어 모듈은 validPost를 추가하엿따.
    validPost는 유효한 게시물인지 확인하는 작업이다.
    validPost는 (1) 제목이 입력됐는지 여부 (2) 내용이 입력됐는지 여부
    를 조사한다.
    
    또한, ./server/routes/write.js에서 작성된 게시물을 업로드하는 코드를 일부 작성하였다.

--- server/middlewares/write.js
+++ server/middlewares/write.js
@@ -1,7 +1,22 @@
 'use strict';
 
-module.exports={
-    vaildPost: (req,res,next)=>{
-        
+module.exports = {
+    vaildPost: (req, res, next) => {
+        const title = req.body.title;
+        const content = req.body.title;
+
+        if (!title) {
+            return res.status(400).send({
+                message: "게시물 제목이 입력되지 않았습니다."
+            });
+        }
+
+        if (!content) {
+            return res.status(400).send({
+                message: "게시물 내용이 입력되지 않았습니다."
+            });
+        }
+
+        next();
     }
 }
--- server/routes/write.js
+++ server/routes/write.js
@@ -3,12 +3,34 @@
 const express = require('express');
 const router = express.Router();
 
+const uuid = require('uuid');
+
 const db = require('../../src/lib/db.js');
 const userMiddleware = require('../middlewares/users.js');
+const writeMiddleware = require('../middlewares/write.js');
 
-router.post('/',userMiddleware.isLoggedIn,(req,res)=>{
-    const {title, contents} = req.body;
-    
+router.post('/', userMiddleware.isLoggedIn, writeMiddleware.vaildPost, (req, res) => {
+    const { title, content } = req.body;
+    // TODO : DB에 게시물 업로드
+    db.query(`
+        INSERT INTO POSTS
+        (pid, uid, title, content)
+        VALUES
+        (${db.escape(uuid.v4())}, ${}, ${db.escape(title)}, ${db.escape(content)})
+    `,(dbErr, dbRes)=>{
+
+        if(dbErr){
+            // db 업로드 과정에서 에러가 났을 때
+            return res.status(500).send({
+                message:"게시물을 업로드하지 못했습니다."
+            });
+        }
+
+        else{
+            // 게시물 작성 후 업로드 완료되면 
+            res.redirect(`http://localhost:${process.env.PORT}`);
+        }
+    });
 });
 
 module.exports = router;
