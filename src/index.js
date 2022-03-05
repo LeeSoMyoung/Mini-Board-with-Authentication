cd0f7d495654d850d8f0f5ffa84ae8c4590e8b13
LeeSoMyoung <so4644009@gmail.com>
2022-03-05 23:27:08 +0900

    fix: 사이드바 클릭시 SPA 미가동 버그 해결
    사이드바의 카테고리를 클릭하면 새로고침 없이 화면 이동이 일어났어야 했다.
    하지만 새로고침이 일어나고 제대로 화면을 못 불러 왔지만, body 부분에 이벤트리스너를 추가하여
    사이드바 메뉴를 클릭할 시 새로고침 없이 이동할 수 있도록 했다.

--- src/index.js
+++ src/index.js
@@ -4,5 +4,11 @@ import { router, navigateTo } from './static/js/routes/router.js';
 
 window.addEventListener('popstate',router);
 document.addEventListener('DOMContentLoaded', ()=>{
+    document.body.addEventListener('click',(event)=>{
+        if(event.target.matches('[data-link]')){
+            event.preventDefault();
+            navigateTo(event.target.href);
+        }
+    });
     router();
 });
